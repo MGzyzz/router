296515902a46cc8ec812bc3942a87ab3264c1edb
Madi <madi_gaziz@mail.ru>
2023-05-28 15:42:05 +0600

    completed_task_one_main_file

---
+++ main_one.py
@@ -0,0 +1,35 @@
+import socketserver
+
+from controllers.pages import PagesController
+from controllers.click import ClickerController
+from router import Router
+from request import Request
+from response import Response
+from static_responder import StaticResponder
+import errors
+
+HOST, PORT = '127.0.0.1', 1025
+
+
+router = Router()
+router.get('/one', PagesController, 'first_page')
+router.get('/two', PagesController, 'second_page')
+router.get('/three', PagesController, 'third_page')
+router.get('/', PagesController, 'list_page')
+
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    def handle(self) -> None:
+        request = Request(file=self.rfile)
+        response = Response(file=self.wfile)
+
+        static_responder = StaticResponder(request, response, 'static')
+
+        router.run(request, response)
+        response.send()
+
+
+socketserver.TCPServer.allow_reuse_address = True
+
+with socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler) as server:
+    server.serve_forever()
